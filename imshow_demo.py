068fa5416765aafc753799c5f7999571bb6e6e45
Evan Cummings <evan.cummings@umontana.edu>
2013-11-05 00:24:04 -0700

    added GNUv3

--- imshow_demo.py
+++ imshow_demo.py
@@ -3,6 +3,22 @@
 # CSCS 444 - Data Structures
 # 9.29.11
 
+#    Copyright (C) <2011>  <cummings.evan@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # Purpose:
 #   Program for displaying a variey of three dimensional data 
 #   highlighting the features of imPlot, ogrid, and the scipy 
